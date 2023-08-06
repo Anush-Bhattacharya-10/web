9de443fca4d724cc0fe8b167f503f951d08d1f6c
Anush Bhattacharya <bhattacharyaanush@gmail.com>
2023-08-06 18:18:20 +0530

    Tabulation error fixed

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ const App = () => {
     return (
         <ChatEngine 
             height="100vh"
-            projectID="6bf4c750-fd6d-4255-8974-3dbca8210879"            userName=""
+            projectID="6bf4c750-fd6d-4255-8974-3dbca8210879"
+            userName=""
             userSecret=""
         />
     )
