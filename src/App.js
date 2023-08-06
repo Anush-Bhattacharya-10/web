a16c6190acc0182aeb2088df0ab2c8b374263211
Anush Bhattacharya <bhattacharyaanush@gmail.com>
2023-08-06 18:17:38 +0530

    Chat Engine config

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import { ChatEngine } from "react-chat-engine";
+import './App.css';
+
+const App = () => {
+    return (
+        <ChatEngine 
+            height="100vh"
+            projectID="6bf4c750-fd6d-4255-8974-3dbca8210879"            userName=""
+            userSecret=""
+        />
+    )
+}
+
+export default App;
